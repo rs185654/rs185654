33bf56580b1d204eba2da566108f873a1cadfb96
Singh, Rovins Singh <rs185654@ncr.com>
2023-01-04 16:28:15 +0530

    added sample files

---
+++ MySampleApp/MySampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySampleApp
+//
+//  Created by Singh, Rovins Singh on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
